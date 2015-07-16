defecf0d5aa1c24a14aca4eb11fd0f613c9f4b16
karthik reddy <karthik.gakkidi@gmail.com>
2015-07-16 10:05:53 -0700

    fixed expired items bug

--- lib/logstash/filters/delta.rb
+++ lib/logstash/filters/delta.rb
@@ -6,8 +6,8 @@ require 'socket'
 require 'logger'
 class LogStash::Filters::Delta < LogStash::Filters::Base
 
-  TIMEOUT = 30*60 #timeout after 30 minutes
-
+  TIMEOUT = 15 #timeout after 30 minutes
+  PERIOD_FLUSH = 5
   config_name "delta"
   
   # start tag, read from config file
@@ -48,7 +48,7 @@ class LogStash::Filters::Delta < LogStash::Filters::Base
       @lock.synchronize do
         #add this event into data store, if it does not exist already
         if !(@ht.has_key?(event[@id_tag]))
-          @ht[event[@id_tag]] = event
+          @ht[event[@id_tag]] = Item.new(event)
         end
         # design choice ?? ignore event with start tag and existing id  or update with data store with new event
         # timestamp will defintelty get updated with option 2
@@ -62,10 +62,10 @@ class LogStash::Filters::Delta < LogStash::Filters::Base
       @logger.info("delta, 'end event' received", end_tag: @end_tag, unique_id_field: @tag_id)
       @lock.synchronize do # has to be here, otherwise inconsistent state may arise
         if(@ht.has_key?(event[@id_tag]))   #if id already exists
-          start_event=@ht[event[@id_tag]]  # delete start event from data store
+          start_item=@ht[event[@id_tag]]  # delete start event from data store
           @ht.delete(event[@id_tag])       
           #take delta between start event and end event
-          delta_t = event["@timestamp"] - start_event["@timestamp"]
+          delta_t = event["@timestamp"] - start_item.event["@timestamp"]
           # generate a new event with delta_t
           new_event=create_new_event(delta_t,@id_tag)
           filter_matched(new_event)       # so that logstash can add fields
@@ -108,6 +108,32 @@ class LogStash::Filters::Delta < LogStash::Filters::Base
   public 
   def flush(options = {})
     puts "flush"
+    @lock.synchronize do
+      @ht.each do |key,item|  #increment all items by 5 ticks
+        item.ticks = item.ticks + PERIOD_FLUSH
+      end #end iterator
+      @ht.each do |key,item|  #delete expired items
+        if(item.ticks >= TIMEOUT)
+          @ht.delete(key)
+          puts "#{key}"
+        else
+          break #since iterator returns FIFO
+        end
+      end #end iterator
+    end #end synchronize
+    
   end
   
 end # class LogStash::Filters::Example
+
+
+class Item
+
+  attr_accessor :event, :ticks
+  
+  def initialize(a)
+    @event = a
+    @ticks = 0
+  end
+
+end
